0294e9d4beaddf6a90385ce8661915703c8d787b
Spencer Melo <su.spencer@hotmail.com>
2026-03-26 22:36:52 -0300

    feat: add support to file input

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import sys
 from src.lexer.lexer import lex
 
 if __name__ == "__main__":
-    input = "(10 (4 2 *) +)"
-    tokens = lex(input)
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} <file>")
+        sys.exit(1)
+
+    with open(sys.argv[1], 'r') as file:
+        content = file.read()
+
+    tokens = lex(content)
     print(tokens)
